b8c30dba7226f37697ba01855206a9558ad87db0
zsdoma <zsolt.doma@gmail.com>
2011-11-30 17:18:28 +0100

    Add base testcases.
    Signed-off-by: zsdoma <zsolt.doma@gmail.com>

--- ContentProviderExampleTest/src/com/reindeermobile/example/test/UserProviderTest.java
+++ ContentProviderExampleTest/src/com/reindeermobile/example/test/UserProviderTest.java
@@ -1,8 +1,13 @@
 package com.reindeermobile.example.test;
 
-import com.reindeermobile.example.providers.UserProvider;
-
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.test.ProviderTestCase2;
+
+import com.reindeermobile.example.providers.UserProvider;
+import com.reindeermobile.example.providers.User.Users;
 
 public class UserProviderTest extends ProviderTestCase2<UserProvider> {
 
@@ -14,4 +19,49 @@ public class UserProviderTest extends ProviderTestCase2<UserProvider> {
         super.setUp();
     }
 	
+	public void testFailQuery(){
+        ContentProvider provider = getProvider();
+
+        Uri uri = UserProvider.CONTENT_URI;
+
+        Cursor cursor = provider.query(uri, null, null, null, null);
+
+        assertNotNull(cursor);
+
+        cursor = null;
+        try {
+            cursor = provider.query(Uri.parse("definitelywrong"), null, null, null, null);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+	
+	public void testQuery(){
+        ContentProvider provider = getProvider();
+
+        Uri uri = UserProvider.CONTENT_URI;
+
+        Cursor cursor = provider.query(uri, null, null, null, null);
+
+        assertNotNull(cursor);
+        
+        String email = "";
+
+        cursor = null;
+        try {
+        	ContentValues values = new ContentValues();
+    		values.put(Users.EMAIL, "example@mail.hu");
+    		values.put(Users.NAME, "example name");
+    		Uri insertUri = provider.insert(Uri.parse("content://" + UserProvider.PROVIDER_NAME + "/" + Users.USERS_TABLE_NAME), values);
+        	
+            cursor = provider.query(Uri.parse("content://" + UserProvider.PROVIDER_NAME + "/" + Users.USERS_TABLE_NAME), null, null, null, null);
+            if (cursor.moveToFirst()) {
+            	email = cursor.getString(cursor.getColumnIndex(Users.EMAIL));
+            }
+            assertEquals("example@mail.hu", email);
+        } catch (IllegalArgumentException e) {
+            assertTrue(false);
+        }
+    }
 }

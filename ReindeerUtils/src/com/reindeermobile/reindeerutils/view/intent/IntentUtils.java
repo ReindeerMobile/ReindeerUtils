750708f02acc9fd3645bfbe3b11b0c0547e43308
zsdoma <zsolt.doma@gmail.com>
2012-02-17 15:46:41 +0100

    Remove method support from IntentParam annotaion. Add getIntentParams method to IntentUilts.

--- ReindeerUtils/src/com/reindeermobile/reindeerutils/view/intent/IntentUtils.java
+++ ReindeerUtils/src/com/reindeermobile/reindeerutils/view/intent/IntentUtils.java
@@ -1,6 +1,7 @@
 package com.reindeermobile.reindeerutils.view.intent;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import java.lang.reflect.Field;
@@ -23,6 +24,7 @@ public class IntentUtils {
 	 * @return Az intent-el tér vissza, ami már tartalmazza a paramétereket.
 	 */
 	public static Intent putIntentParams(Intent intent, Object obj) {
+		Log.d(TAG, "putIntentParams - START");
 		if (intent == null) {
 			throw new IllegalArgumentException("Intent is null!");
 		}
@@ -34,58 +36,141 @@ public class IntentUtils {
 		Field[] fields = obj.getClass().getDeclaredFields();
 		for (int i = 0; i < fields.length; i++) {
 			Field field = fields[i];
+
 			if (field.isAnnotationPresent(IntentParam.class)) {
 				IntentParam ann = field.getAnnotation(IntentParam.class);
 				String name = ann.name();
-				String getterMethodName = "get"
-						+ field.getName().substring(0, 1).toUpperCase()
+
+				if (name == null || name.length() == 0) {
+					name = field.getName();
+				}
+
+				String methodNamePostfix = field.getName().substring(0, 1)
+						.toUpperCase()
 						+ field.getName().substring(1);
+				String getterMethodName = "get" + methodNamePostfix;
+				String setterMethodName = "set" + methodNamePostfix;
 
-				Object value = getMethodValue(getterMethodName, obj);
-				intent = putIntentParam(intent, name, value, field.getType());
+				try {
+					Method getter = obj.getClass().getMethod(getterMethodName,
+							new Class[] {});
+					@SuppressWarnings("unused")
+					// Test reason only
+					Method setter = obj.getClass().getMethod(setterMethodName,
+							field.getType());
+
+					Object value = getter.invoke(obj, new Object[] {});
+					intent = putIntentParam(intent, name, value,
+							field.getType());
+				} catch (SecurityException exception) {
+					Log.e(TAG, "putIntentParams - ", exception);
+				} catch (IllegalArgumentException exception) {
+					Log.e(TAG, "putIntentParams - ", exception);
+					throw exception;
+				} catch (NoSuchMethodException exception) {
+					Log.w(TAG, "putIntentParams - ", exception);
+				} catch (IllegalAccessException exception) {
+					Log.e(TAG, "putIntentParams - ", exception);
+				} catch (InvocationTargetException exception) {
+					Log.e(TAG, "putIntentParams - ", exception);
+				}
 			}
 		}
 
-		// Annotált metódusok feldolgozása
-		Method[] methods = obj.getClass().getMethods();
-		for (int i = 0; i < methods.length; i++) {
-			Method method = methods[i];
+		/*
+		 * Nem kell, mert kötelező a getter-setter az adattaghoz, így elég az
+		 * adattagból kiindulva meghívni a metódusokat.
+		 */
+		/*
+		 * Annotált metódusok feldolgozása
+		 */
+		// Method[] methods = obj.getClass().getMethods();
+		// for (int i = 0; i < methods.length; i++) {
+		// Method method = methods[i];
+		//
+		// String fieldName =
+		//
+		// if (method.isAnnotationPresent(IntentParam.class) &&
+		// method.getName().startsWith("get")) {
+		// IntentParam ann = method.getAnnotation(IntentParam.class);
+		// String name = ann.name();
+		// Object value = getMethodValue(method.getName(), obj);
+		// intent = putIntentParam(intent, name, value,
+		// method.getReturnType());
+		// }
+		//
+		// }
 
-			if (method.isAnnotationPresent(IntentParam.class)) {
-				IntentParam ann = method.getAnnotation(IntentParam.class);
-				String name = ann.name();
-				Object value = getMethodValue(method.getName(), obj);
-				intent = putIntentParam(intent, name, value,
-						method.getReturnType());
-			}
-
-		}
-
+		Log.d(TAG, "putIntentParams - END");
 		return intent;
 	}
-
-	private static Object getMethodValue(String methodName, Object obj) {
-		Object value = null;
+	
+	public static <T> T getIntentParams(Intent intent, Class<T> resultClass) {
+		Log.d(TAG, "getIntentParams - START");
+		if (intent == null) {
+			throw new IllegalArgumentException("The intent is null!");
+		}
+		if (resultClass == null) {
+			throw new IllegalArgumentException("The resultClass is null!");
+		}
+		
+		T result = null;
 		try {
-			Method method = obj.getClass()
-					.getMethod(methodName, new Class[] {});
-			value = method.invoke(obj, new Object[] {});
+			result = resultClass.newInstance();
+			Log.d(TAG, "getIntentParams - resultClass" + result.toString());
+			Bundle bundle = intent.getExtras();
+			if (bundle != null) {
+				Field[] fields = resultClass.getDeclaredFields();
+				for (Field field : fields) {
+					try {
+						if (field.isAnnotationPresent(IntentParam.class)) {
+							IntentParam intentParam = field
+									.getAnnotation(IntentParam.class);
+							String name = intentParam.name();
+
+							if (name == null || name.length() == 0) {
+								name = field.getName();
+							}
+
+							String methodNamePostfix = field.getName()
+									.substring(0, 1).toUpperCase()
+									+ field.getName().substring(1);
+							String setterMethodName = "set" + methodNamePostfix;
+
+							Method setter = resultClass.getMethod(
+									setterMethodName, field.getType());
+
+							Object value = null;
+							if (field.getType() == String.class) {
+								value = bundle.getString(name);
+								setter.invoke(result, value);
+							}
+						}
+					} catch (NoSuchMethodException exception) {
+						Log.w(TAG, "getIntentParams - :", exception);
+					} catch (IllegalArgumentException exception) {
+						Log.w(TAG, "getIntentParams - :", exception);
+					} catch (InvocationTargetException exception) {
+						Log.w(TAG, "getIntentParams - :", exception);
+					}
+				}
+			}
 		} catch (SecurityException exception) {
-			Log.w(TAG, "getMethodValue - ", exception);
-		} catch (NoSuchMethodException exception) {
-			Log.w(TAG, "getMethodValue - ", exception);
-		} catch (IllegalArgumentException exception) {
 			Log.w(TAG, "getMethodValue - ", exception);
 		} catch (IllegalAccessException exception) {
 			Log.w(TAG, "getMethodValue - ", exception);
-		} catch (InvocationTargetException exception) {
+		} catch (InstantiationException exception) {
 			Log.w(TAG, "getMethodValue - ", exception);
 		}
-		return value;
+		
+		Log.d(TAG, "getIntentParams - END");
+
+		return result;
 	}
 
 	private static Intent putIntentParam(Intent intent, String name,
 			Object value, Class<? extends Object> clazz) {
+		Log.d(TAG, "putIntentParam - START");
 		if (clazz == String.class) {
 			intent.putExtra(name, (String) value);
 		} else if (clazz == Integer.class) {
@@ -93,6 +178,7 @@ public class IntentUtils {
 		} else if (clazz == Boolean.class) {
 			intent.putExtra(name, (Boolean) value);
 		}
+		Log.d(TAG, "putIntentParam - END");
 		return intent;
 	}
 }

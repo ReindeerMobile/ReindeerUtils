a03ce285ed663c01bf290f144bbbdc4f9900fbc8
zsdoma <zsolt.doma@gmail.com>
2012-05-30 16:59:11 +0200

    Remove MVP and ORM sources from RMUtils.

--- ReindeerUtils/src/com/reindeermobile/reindeerutils/mvp/IModel.java
+++
@@ -1,8 +0,0 @@
-package com.reindeermobile.reindeerutils.mvp;
-
-import android.content.Context;
-import android.os.Handler.Callback;
-
-public interface IModel extends Callback {
-	void init(Context context);
-}
--- ReindeerUtils/src/com/reindeermobile/reindeerutils/mvp/IView.java
+++
@@ -1,9 +0,0 @@
-package com.reindeermobile.reindeerutils.mvp;
-
-import com.reindeermobile.reindeerutils.mvp.ViewHandler.IViewTask;
-
-public interface IView {
-	void registerViewTask(int serviceId, IViewTask viewTask);
-
-	void registerViewTask(String serviceName, IViewTask viewTask);
-}
